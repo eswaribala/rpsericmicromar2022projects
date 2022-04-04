16e62c85b0de8d93684b31934c534844ab49bedf
eswaribala <parameswaribala@gmail.com>
2022-04-04 11:44:15 +0530

    updated

---
+++ ericgateway/src/main/java/com/eric/ericgateway/EricgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.eric.ericgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EricgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EricgatewayApplication.class, args);
+	}
+
+}
---
+++ ericgateway/src/test/java/com/eric/ericgateway/EricgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.ericgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EricgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
