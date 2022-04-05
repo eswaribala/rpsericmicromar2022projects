3012e58403f296a71afd46ea7bfddae232249a49
eswaribala <parameswaribala@gmail.com>
2022-04-05 10:43:36 +0530

    updated

---
+++ ericgateway/src/main/java/com/eric/ericgateway/vos/JwtResponse.java
@@ -0,0 +1,17 @@
+package com.eric.ericgateway.vos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//DTO
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtResponse {
+
+	private String token;
+
+	
+
+}
